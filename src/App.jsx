b07d5da3e4a26763b6dccfe150d49d7c7fc81176
Abishlal <abishlalns03@gmail.com>
2022-07-11 16:10:18 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const val = () =>{
+  return(
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p> 
+      <h1>hello abish</h1>
+    </>
+  )
+}
+
+export default  val;
